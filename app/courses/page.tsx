798120f469919d187be837c70119f201adbb43bd
Akanksha Singh <akanskahsingh0085@gmail.com>
2025-07-21 21:09:30 +0530

    all good in courses page

--- app/courses/page.tsx
+++ app/courses/page.tsx
@@ -96,7 +96,7 @@ const CoursesPage: React.FC = () => {
     <div className="flex justify-center items-center py-20">
       <div className="relative">
         <div className="w-12 h-12 rounded-full border-4 border-gray-200"></div>
-        <div className="w-12 h-12 rounded-full border-4 border-[#7AC86B] border-t-transparent animate-spin absolute top-0 left-0"></div>
+        <div className="w-12 h-12 rounded-full border-1 border-[#7AC86B] border-t-transparent animate-spin absolute top-0 left-0"></div>
       </div>
     </div>
   );
